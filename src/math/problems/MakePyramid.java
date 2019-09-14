9517a93b07ecd2e7e6de4b9cd5b7a34ce4d2beaa
kabir <kajol11373@gmail.com>
2019-09-14 02:08:15 -0400

    definition answer

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -3,16 +3,16 @@ package math.problems;
 public class MakePyramid {
     public static void main(String[] args) {
 
-        /*   Implement a large Pyramid of stars in the screen with java.
-
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
-
-        */
+//        /*   Implement a large Pyramid of stars in the screen with java.
+//
+//                              *
+//                             * *
+//                            * * *
+//                           * * * *
+//                          * * * * *
+//                         * * * * * *
+//
+//        */
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6 - i; j++) {
